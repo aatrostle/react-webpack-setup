6b86f922ecd4d569a33fc5ea212aefb9860fe62e
Aaron Trostle <aaron.trostle@gmail.com>
2015-10-18 20:52:27 -0700

    Adding babel-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ module.exports = {
   context: __dirname + "/app",
   entry: "./app.js",
 
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["babel-loader"],
+    }
+  ],
+
   output: {
     filename: "app.js",
     path: __dirname + "/dist",
